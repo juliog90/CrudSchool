7a8bc7c0f1915fe11d2f562c1711e4b28d9482d6
Julio Gastelum <tjuliogastelum90@gmail.com>
2019-10-07 21:48:51 -0700

    Base de Datos SQL

--- PersonasDB.sql
+++ PersonasDB.sql
@@ -1,25 +1,79 @@
 -- Crud de Personas MySQL Codigo SQL para la creacion de la base de datos
 
-create database if not exists escuela;
+drop database if exists escuela;
+create database escuela;
 
 use escuela;
 
-create table Personas (
+create table `Carreras` (
+    `id` int not null auto_increment,
+    `nombre` varchar(40) not null,
+    primary key(`id`)
+);
+
+create table `Profesor` (
+    `id` int not null auto_increment,
+    `nombre` varchar(40) not null,
+    `grado` varchar(30) not null,
+    primary key(`id`)
+);
+
+create table `Materias` (
+    `id` int not null auto_increment,
+    `nombre` varchar(40) not null,
+    primary key(`id`)
+);
+
+create table `Cursos` (
     id int not null auto_increment,
     nombre varchar(40) not null,
-    apellido_paterno varchar(40) not null,
-    apellido_materno varchar(40)
-)
+    carrera_id int,
+    constraint fk_cursoscarreras
+    foreign key (carrera_id) references Carreras(id),
+    primary key(`id`)
+);
 
-create table Alumnos (
-    id int not null auto_increment,
-    carrera varchar(40) not null,
-    grupo varchar(20)
-)
+create table `Grupos` (
+    `id` int not null auto_increment,
+    `nombre` varchar(4) not null,
+    `curso_id` int not null,
+    primary key(`id`),
+    foreign key (`curso_id`) references Cursos(`id`)
+);
 
-create table Carreras (
-    id int not null auto_increment,
-    nombre varchar(40) not null
-)
+create table `Alumnos` (
+    `id` int not null auto_increment,
+    `nombre` varchar(40) not null,
+    `apellido_paterno` varchar(40) not null,
+    `apellido_materno` varchar(40),
+    `grupo_id` int not null,
+    primary key(`id`),
+    foreign key (`grupo_id`) references Grupos(`id`)
+);
 
+create table `MateriasProfesor` (
+    `id` int not null auto_increment,
+    `materia_id` int not null,
+    `profesor_id` int not null,
+    primary key(`id`),
+    foreign key(`profesor_id`) references Profesor(`id`),
+    foreign key(`materia_id`) references Materias(`id`)
+);
 
+create table `CursosMaterias` (
+    `id` int not null auto_increment,
+    `curso_id` int not null,
+    `materia_id` int not null,
+    foreign key(`curso_id`) references Cursos(`id`),
+    foreign key(`materia_id`) references Materias(`id`),
+    primary key(`id`)
+);
+
+create table `GruposCursos` (
+    `id` int not null auto_increment,
+    `grupo_id` int not null,
+    `curso_id` int not null,
+    foreign key(`grupo_id`) references Grupos(`id`),
+    foreign key(`curso_id`) references Cursos(`id`),
+    primary key(`id`)
+);
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-
+        try{
+            Class.forName("org.mariadb.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/sonoo","root","");
+//here sonoo is database name, root is username and password
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery("select * from Carreras");
+            while(rs.next())
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
     }
 }
